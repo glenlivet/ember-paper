98d3520352c7f6ad4ddf3769c20f4004eaaaff1d
Miguel Andrade <jmandrade0@gmail.com>
2018-10-06 16:55:59 +0100

    wip paper-datepicker

---
+++ addon/components/paper-calendar-days.js
@@ -0,0 +1,7 @@
+import PowerCalendarDaysComponent from 'ember-power-calendar/components/power-calendar/days';
+import layout from '../templates/components/paper-calendar-days';
+
+export default PowerCalendarDaysComponent.extend({
+  layout,
+  tagName: 'md-calendar-month'
+});
